87983b07f3be6bc54bb6673d981c37ade316088f
jatashankar <jspatel08@gmail.com>
2018-05-29 23:01:59 +0530

    productlist component comit

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -34,7 +34,7 @@ listFilterby : string = 'Cart';
 //array<any> product = [] /// we acan also write such a way
 
     products : IProduct[] = [{
-        productName: "RedRose",       
+        productName: "RedRose New",       
         code : "101",
         available: "yes",  
         price: 3000,       
