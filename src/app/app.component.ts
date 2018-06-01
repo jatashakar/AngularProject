2ed6b32b9f476b1b93eb674d0d0e693070603edb
jatashankar <jspatel08@gmail.com>
2018-06-01 17:38:04 +0530

    Gitacora saved 1 changed files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { ProductserviceService } from './service/productservice.service';
+import { PserviceService } from './pservice.service';
 
 @Component({
   selector: 'app-root',
   template:`<product-list></product-list>`,
   //templateUrl: './app.component.html',
   //styleUrls: ['./app.component.css']
-  providers : [ProductserviceService],
+  providers : [ProductserviceService, PserviceService]
 })
 export class AppComponent {
   title = 'app...................';
