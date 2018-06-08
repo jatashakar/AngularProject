384b9734e2439ced2c9242e2323e14a49cc00805
jatashankar <jspatel08@gmail.com>
2018-06-08 10:08:46 +0530

    Gitacora saved 1 changed files

--- src/app/service/productservice.service.ts
+++ src/app/service/productservice.service.ts
@@ -21,9 +21,7 @@ export class ProductserviceService {
 getProduct() : Observable<IProduct[]>{
    return this._httpclint.get<IProduct[]>(this._productUrl)
           .do(data => console.log('All Data:'+JSON.stringify(data)))
-          .catch(this.handleError);
-  
-}
+          .catch(this.handleError);}
   // getProduct():  IProduct[]{
 
   //   console.log ("service strat 2222");
