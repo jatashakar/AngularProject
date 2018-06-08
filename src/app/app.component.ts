4b8470ffd2fc45549ece9942773152ad3e12f4b8
jatashankar <jspatel08@gmail.com>
2018-06-08 20:11:32 +0530

    Gitacora saved 4 changed files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,22 @@ import { ProductserviceService } from './service/productservice.service';
 
 @Component({
   selector: 'app-root',
-  template:`<product-list>`,
+  template:`
+  <div>
+<nav class ='navbar navbar-default'>
+<div class = 'conrainer-fluid'>
+<a class='navbar-brand'>{{pageTitle}}</a>
+<ul class = 'nav navbar-nav'>
+<li><a [routerLink]="['/products']">Home</a></li>
+<li><a [routerLink]="['/products']">Product List</a></li>
+</ul>
+</div>
+</nav>
+<div class= 'container'>
+<router-outlet></routrer-outlet>
+</div>
+  </div>
+  `,
   //templateUrl: './app.component.html',
   //styleUrls: ['./app.component.css']
   providers : [ProductserviceService]
